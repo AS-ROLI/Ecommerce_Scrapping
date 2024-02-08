8770e22a0c1624914310e81f00e8633aa430d7a5
Ayush Singroly <92685405+AS-ROLI@users.noreply.github.com>
2024-02-08 13:19:52 +0530

    Update application.py

--- application.py
+++ application.py
@@ -142,6 +142,6 @@ def index():
     else:
         return render_template('index.html')
 
-if __name__ == "__main__":
-    app.run(host='127.0.0.1', port=8000, debug=True)
+#if __name__ == "__main__":
+    #app.run(host='127.0.0.1', port=8000, debug=True)
 	#app.run(debug=True)    
